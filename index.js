b0780cb5ec4284de549725cb7e34b43442e90912
Nicolas <nicolas.sirac@orange.fr>
2016-06-01 17:37:55 +0200

    final

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var api = express();
-
+var DEFAULT_PORT = 3000;
 api.get('/',function(req,res,next){
-	res.send('Hello World');
+	res.send('Hello world');
 });
 
-console.log('server start port 3000');
-api.listen(3000);
+api.get('/contacts',function(req,res,next){
+	res.send([]);
+});
+api.get('/contacts/:name',function(req,res,next){
+	res.send();
+});
+api.post('/contacts/:name',function(req,res,next){
+	if(req.params.name === 'exist')
+		return res.status(403).send();
+	res.send();
+});
+api.put('/contacts/:name/:new',function(req,res,next){
+	res.send();
+});
+api.delete('/contacts/:name',function(req,res,next){
+	res.send();
+});
+
+var port = process.env.PORT || DEFAULT_PORT
+console.log('server start port '+port);
+api.listen(port);
 
 module.exports = api;
--- test/index.specs.js
+++ test/index.specs.js
@@ -1,26 +1,46 @@
 var request = require('supertest');
-var api= require('../index');
+var api = require('../index');
 
-describe('contact',function(){
-	describe('GET/contacts',function(){
-		return request(api).get('/contacts').send().expect(200);
+describe('contacts',function(){
+	describe('GET /contacts',function(){
+		it('should return 200',function(){
+			return request(api)
+			.get('/contacts')
+			.send()
+			.expect(200)
+		});
 	});
+	
+	describe('POST /contacts/:name',function(){
+		it('should create a new contact',function(){
+			return request(api)
+			.post('/contacts/foo')
+			.send()
+			.expect(200)
+		});
+		it('should not be authorized to create a new contact with an already ',function(){
+			return request(api)
+			.post('/contacts/exist')
+			.send()
+			.expect(403)
+		});
+	});
+	describe('PUT /contacts/:name/:new',function(){
+		it('should cupdtae contact with same name',function(){
+			return request(api)
+			.put('/contacts/foo/bar')
+			.send()
+			.expect(200)
+		});
+	});
+	describe('DELETE /contacts/:name',function(){
+		it('should remove a new contact',function(){
+			return request(api)
+			.delete('/contacts/foo')
+			.send()
+			.expect(200)
+		});
+	});
+	
+
 });
-describe('POST /contacts/:name',function(){
-	it('should create a new contact',function(){
-		return request(api).post('/contacts/foo').send().expect(200)
-	})
-	it('should not be authorized to create a new contact with an already',function(){
-		return request(api).post('/contacts/foo').send().expect(403)
-	})
-})
-describe('PUT /contacts/:name/:new',function(){
-	it('should update contacts with the same name',function(){
-		return request(api).put('/contacts/foo/bar').send().expect(200)
-	})
-})
-describe('DELETE /contacts/:name/',function(){
-	it('should remove contacts with the same name',function(){
-		return request(api).delete('/contacts/foo/').send().expect(200)
-	})
-})
